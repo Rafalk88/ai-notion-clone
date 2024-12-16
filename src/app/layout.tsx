498bbcc1e1a9072eb4bbc269e440f928e8a08acf
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-12-16 22:30:32 +0100

    feat: move Header to file & add blank test file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "@/components";
 
 export const metadata: Metadata = {
   title: "AI notion clone",

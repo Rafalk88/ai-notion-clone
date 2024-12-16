80d9940fbb44355a7c8911a70dffa232440c2b71
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-12-16 21:39:02 +0100

    chore: clerk instalation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="">
       <h1>Lets build an API productivity APP</h1>
+      
+      <Button>Click Me</Button>
     </main>
   );
 }

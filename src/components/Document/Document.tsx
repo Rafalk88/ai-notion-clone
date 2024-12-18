e4827685af16ea2350c0fd25ee6e8cc960d395d9
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-12-18 21:22:00 +0100

    CI: add change title input

--- src/components/Document/Document.tsx
+++ src/components/Document/Document.tsx
@@ -1,7 +1,61 @@
 "use client"
 
+import { FormEvent, useState, useTransition, useEffect, useCallback } from "react"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/../firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
 export default function Document({ id }: { id: string }) {
+  const [data] = useDocumentData(doc(db, "documents", id));
+  const [input, setInput] = useState('');
+  const [isUpdating, startTransition] = useTransition();
+
+  const updateTitle = useCallback((e: FormEvent) => {
+    e.preventDefault();
+
+    if (input.trim()) {
+      startTransition(async () => {
+        await updateDoc(doc(db, "documents", id), {
+          title: input,
+        })
+      });
+    }
+  }, [id, input]);
+
+  useEffect(() => {
+    if (data) {
+      setInput(data.title);
+    }
+  }, [data])
+
   return (
-    <section>Document</section>
+    <section>
+      <div className="flex max-w-6xl mx-auto justify-between">
+        <form className="flex flex-1 space-x-2">
+          {/* update title... */}
+          <Input value={input} onChange={(e) => setInput(e.target.value)}/>
+
+          <Button
+            disabled={isUpdating}
+            type="submit"
+            onClick={updateTitle}
+          >
+            {isUpdating ? "Updating..." : "Update"}
+          </Button>
+
+          {/* if statement */}
+          {/* isOwner && InviteUser, DeleteDocument */}
+        </form>
+      </div>
+      <div>
+        {/* ManageUsers */}
+
+        {/* Avatars */}
+      </div>
+
+      {/* Collaborative Editor */}
+    </section>
   )
 }

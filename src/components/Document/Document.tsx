00326e5268cc4dd8e822776e23648097c81f25fa
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-12-18 20:52:12 +0100

    feat(/doc): add basic components

---
+++ src/components/Document/Document.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+export default function Document({ id }: { id: string }) {
+  return (
+    <section>Document</section>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { Sidebar, SidebarOption } from './Sidebar'
 export { default as NewDocumentButton } from './NewDocumentButton';
+export { Document } from './Document';
